a529feea62fe57796e0077960635f9a0bea51dc0
Vadim K. <vadim@vadirn.io>
2024-10-17 16:44:31 +0500

    extract home manager config

--- flake.nix
+++ flake.nix
@@ -6,82 +6,64 @@
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     alejandra.url = "github:kamadorueda/alejandra/3.0.0";
     alejandra.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ {
     self,
-    nix-darwin,
     nixpkgs,
+    nix-darwin,
     nix-homebrew,
+    home-manager,
     alejandra,
   }: let
+    system = "aarch64-darwin";
     configuration = {pkgs, ...}: {
       nixpkgs.config.allowUnfree = true;
 
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
       environment.systemPackages = [
         pkgs.git
-        pkgs.oh-my-zsh
         pkgs.obsidian
         pkgs.iina
-        pkgs.nil # nix LSP for VSCode
-        pkgs.alejandra # nix formatter for VSCode
+        pkgs.nil
+        pkgs.alejandra
       ];
 
       homebrew = {
         enable = true;
-        brews = [
-          "mas"
-        ];
-        casks = [
-          "raycast"
-          "imageoptim"
-          "qmk-toolbox"
-          "firefox"
-        ];
-        masApps = {
+        brews = ["mas"];
+        casks = ["raycast" "imageoptim" "qmk-toolbox" "firefox"];
+        masApps = {};
+        onActivation = {
+          # cleanup = "zap";
+          # autoUpdate = true;
+          # upgrade = true;
         };
-        # onActivation.cleanup = "zap";
-        onActivation.autoUpdate = true;
-        onActivation.upgrade = true;
       };
 
       system.defaults = {
         dock.autohide = true;
-        # dock.persistent-apps = [];
         finder.FXPreferredViewStyle = "clmv";
         loginwindow.GuestEnabled = false;
         NSGlobalDomain.AppleICUForce24HourTime = true;
       };
 
-      # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true; # default shell on catalina
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
+      programs.zsh.enable = true;
       system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
       system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+    homeConfiguration = import ./home {
+      username = "vadim";
+      homeDirectory = "/Users/vadim";
     };
   in {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#studio
-    darwinConfigurations."studio" = nix-darwin.lib.darwinSystem {
+    darwinConfigurations.studio = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
       modules = [
         configuration
         nix-homebrew.darwinModules.nix-homebrew
@@ -93,13 +75,12 @@
             autoMigrate = true;
           };
         }
+        home-manager.darwinModules.home-manager
+        homeConfiguration
       ];
     };
 
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."studio".pkgs;
-
-    # Specify the formatter
-    formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.alejandra;
+    darwinPackages = self.darwinConfigurations.studio.pkgs;
+    formatter.aarch64-darwin = nixpkgs.legacyPackages.${system}.alejandra;
   };
 }
