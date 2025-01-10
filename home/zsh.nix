ed9834efac5c05f581cb408cd672711fcd1c0808
Vadim Kotov <vadim@vadirn.io>
2025-01-10 09:21:09 +0500

    use brew zed

--- home/zsh.nix
+++ home/zsh.nix
@@ -26,9 +26,9 @@
     };
 
     initExtra = ''
-      ssh-add ~/.ssh/github 2> /dev/null
-      ssh-add ~/.ssh/bitbucket 2> /dev/null
-      ssh-add ~/.ssh/vultr 2> /dev/null
+      ssh-add --apple-use-keychain ~/.ssh/github 2> /dev/null
+      ssh-add --apple-use-keychain ~/.ssh/bitbucket 2> /dev/null
+      ssh-add --apple-use-keychain ~/.ssh/vultr 2> /dev/null
 
       eval $(brew shellenv)
     '';
