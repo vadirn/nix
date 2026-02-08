fcf07d036a5f613f84141057f7df582ea23005f8
Vadim Kotov <vadim@vadirn.io>
2026-02-08 20:41:50 +0500

    feat: catppuccin rounded status bar with mouse copy support

--- home/tmux.nix
+++ home/tmux.nix
@@ -7,16 +7,27 @@
 
     plugins = with pkgs.tmuxPlugins; [
       yank
-      catppuccin
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavor "latte"
+          set -g @catppuccin_window_status_style "rounded"
+          set -g @catppuccin_status_left_separator ""
+          set -g @catppuccin_status_middle_separator ""
+          set -g @catppuccin_status_right_separator "#[fg=#ccd0da,bg=#e6e9ef]#[none]"
+        '';
+      }
     ];
 
     extraConfig = ''
+      set -g status-right-length 100
+      set -g status-left " "
+      set -g status-right "#{E:@catppuccin_status_session} "
+
       bind | split-window -h
       bind _ split-window -v
+      bind -T copy-mode MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "pbcopy"
 
-      set -g @catppuccin_flavor "latte"
-      set -g status-left ""
-      set -g status-right '#[fg=white]#[bold]%H:%M'
       set -g default-command "reattach-to-user-namespace -l $SHELL"
     '';
   };
