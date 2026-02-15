57371fe16dd757d52d7bb1716d6a3f6899c672f6
Vadim Kotov <vadim@vadirn.io>
2026-02-15 23:57:12 +0500

    feat: show hidden files by default in yazi

--- home/yazi.nix
+++ home/yazi.nix
@@ -1,16 +1,17 @@
 {...}: {
   programs.yazi = {
     enable = true;
+    settings.manager.show_hidden = true;
     theme = {
       mode = {
         normal_main = {
-          bg = "blue";
-          fg = "white";
+          bg = "magenta";
+          fg = "0";
           bold = true;
         };
         normal_alt = {
-          bg = "blue";
-          fg = "white";
+          bg = "magenta";
+          fg = "0";
         };
       };
     };
