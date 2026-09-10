be849232e963435f64e2e7ae3c11ceac328bb7d7
Vadim Kotov <vadim@vadirn.io>
2026-09-10 10:02:05 +0500

    fix: skip worktree-checked-out branches in git-cleanup

--- home/zsh.nix
+++ home/zsh.nix
@@ -168,7 +168,9 @@
         echo '--- pruning stale remote-tracking refs ---'
         git remote prune origin
         echo '--- deleting local branches whose upstream is gone ---'
-        git branch -vv | grep ': gone]' | grep -v '\*' | awk '{ print ''$1; }' | xargs -I B git branch -D "B"
+        git for-each-ref --format='%(refname:short) %(upstream:track) %(worktreepath)' refs/heads \
+          | awk '$2 == "[gone]" && $3 == "" { print $1 }' \
+          | xargs -I B git branch -D "B"
         echo '--- garbage-collecting rerere cache ---'
         git rerere gc
       }
